66ed6422d072d7604ef890f7561fd9f70748cd61
Pingu <crackman4212@gmail.com>
2018-04-06 01:00:52 -0700

    App

---
+++ main.js
@@ -0,0 +1,17 @@
+const electron = require('electron');
+const app = electron.app;
+
+const path = require('path');
+const url = require('url');
+
+const BrowserWindow = electron.BrowserWindow;
+
+var mainWindow;
+app.on('ready', function() {
+	mainWindow = new BrowserWindow({width: 300, height: 540, backgroundColor: '#2e2c29'});
+	mainWindow.loadURL(url.format ({
+		pathname: path.join(__dirname, 'calculator.html'),
+		protocol: 'file',
+		slashes: true
+	}));
+});
