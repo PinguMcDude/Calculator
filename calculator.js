b598d3ff1f700510f1914e2ee2b8a12da6f91d88
PinguMcDude <crackman4212@gmail.com>
2018-04-06 15:02:34 -0500

    k

--- calculator.js
+++ calculator.js
@@ -1,6 +1,8 @@
 document.getElementById("answerBox").value = "";
 document.getElementById("inputBox").value = "";
 
+var answerList = [];
+
 function getNumber (x) {
 	var output = document.getElementById("inputBox");
 	output.value += x;
@@ -9,11 +11,21 @@ function getNumber (x) {
 function calculate () {
 	boxValue = document.getElementById("inputBox").value;
 	answer = eval(boxValue);
+	var column = document.getElementById("answersSec");
+	column.innerHTML += boxValue + " = ";
 	document.getElementById("answerBox").value = answer;
 	document.getElementById("inputBox").value = "";
+	column.innerHTML += answer + "<br>";
+
 }
 
 function reset() {
 	document.getElementById("answerBox").value = "";
 	document.getElementById("inputBox").value = "";
+}
+
+function delChar() {
+	input = document.getElementById("inputBox").value;
+	output = input.slice(0, -1);
+	document.getElementById("inputBox").value = output;
 }
